14e8242f993fcd7f4a2b58cd9c25ee3672bb64d3
Pepperolls <rares.mladin@gmail.com>
2021-07-29 18:58:52 +0300

    Created methods and used examples for each parameter modifier

--- C# Basics - 1st assignment/FirstAssignment/HelloWorld/Program.cs
+++ C# Basics - 1st assignment/FirstAssignment/HelloWorld/Program.cs
@@ -2,6 +2,11 @@
 
 namespace HelloWorld
 {
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
     public struct Time
     {
         public int Hour;
@@ -41,18 +46,77 @@ namespace HelloWorld
     }
     class Program
     {
+        static void MethodForNone(int x, Time time)
+        {
+            x += 10;
+            time.Hour += 10;
+            time.Minutes += 10;
+            time.Seconds += 10;
+
+            Console.WriteLine("The number inside the method: " + x);
+            Console.WriteLine($"The time inside the method: {time}");
+        }
+
+        static void MethodForRef(ref int x, ref Time time)
+        {
+            x += 10;
+            time.Hour += 10;
+            time.Minutes += 10;
+            time.Seconds += 10;
+
+            Console.WriteLine("The number inside the method: " + x);
+            Console.WriteLine($"The time inside the method: {time}");
+        }
+
+        static void MethodForOut(out int x, out Time time)
+        {
+            x = 99;
+            time = new Time {Hour = 15, Minutes = 15, Seconds = 15 };
+
+            Console.WriteLine("The number inside the method: " + x);
+            Console.WriteLine($"The time inside the method: {time}");
+        }
+
         static void Main(string[] args)
         {
-            Time now = new Time {Hour = 15, Minutes = 36, Seconds = 48 };
-            Console.WriteLine($"The time is: {now}");
+            Console.WriteLine("Method for no modifier (passed by value):");
 
-            string Make = Console.ReadLine();
-            string Model = Console.ReadLine();
-            string FuelType = Console.ReadLine();
-            double EngineCC = Convert.ToDouble(Console.ReadLine());
+            int x = 10;
+            Time time = new Time { Hour = 10, Minutes = 10, Seconds = 10 };
+            Console.WriteLine("The number before: " + x);
+            Console.WriteLine($"The time before: {time}\n");
 
-            Car car = new Car(Make, Model, FuelType, EngineCC);
-            Console.WriteLine($"\n{car}");
+            MethodForNone(x, time);
+
+            Console.WriteLine("\nThe number after: " + x);
+            Console.WriteLine($"The time after: {time}");
+            //Variabilele cu care se lucreaza in interiorul metodei sunt doar copii, motiv pentru care cele din main raman nemodificate in urma apelului.
+
+            Console.WriteLine("\nMethod for ref modifier (passed by reference):");
+
+            Console.WriteLine("The number before: " + x);
+            Console.WriteLine($"The time before: {time}\n");
+
+            MethodForRef(ref x, ref time);
+
+            Console.WriteLine("\nThe number after: " + x);
+            Console.WriteLine($"The time after: {time}\n");
+            //De data aceasta, variabilele sunt transmise prin referinta, deci x-ul si time-ul din interiorul metodei ajung sa pointeze la adresa din memorie
+            //a variabilelor initiale. Astfel, modificand in metoda, se schimba si in memorie, nu se creaza o copie ca si la transmiterea prin valoare.
+
+
+            Console.WriteLine("\nMethod for out modifier (passed by reference):");
+
+            int y;
+            Time time1 = new Time();
+
+            Console.WriteLine("The number is not assigned before the method");
+            Console.WriteLine($"The time before: {time1}\n");
+
+            MethodForOut(out x, out time);
+            Console.WriteLine("\nThe number after: " + x);
+            Console.WriteLine($"The time after: {time}\n");
+            //Singura diferenta este faptul ca transmiterea prin modificatorul out ne permite sa folosim parametri unassigned inainte de apelul metodei.
         }
     }
 }
