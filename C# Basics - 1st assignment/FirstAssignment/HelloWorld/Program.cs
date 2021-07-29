1f807950d4750d8d7605d1055977184cf00a3cbd
Pepperolls <rares.mladin@gmail.com>
2021-07-29 17:16:29 +0300

    Created instances of value/reference types

--- C# Basics - 1st assignment/FirstAssignment/HelloWorld/Program.cs
+++ C# Basics - 1st assignment/FirstAssignment/HelloWorld/Program.cs
@@ -2,11 +2,52 @@
 
 namespace HelloWorld
 {
+    public struct Time
+    {
+        public int Hour;
+        public int Minutes;
+        public int Seconds;
+
+        public Time(int Hour, int Minutes, int Seconds)
+        {
+            this.Hour = Hour;
+            this.Minutes = Minutes;
+            this.Seconds = Seconds;
+        }
+        public override string ToString()
+        {
+            return $"{Hour}:{Minutes}:{Seconds}";
+        }
+    }
+
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public string FuelType;
+        public double EngineCC;
+
+        public Car(string Make, string Model, string FuelType, double EngineCC)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.FuelType = FuelType;
+            this.EngineCC = EngineCC;
+        }
+        public override string ToString()
+        {
+            return $"Make: {Make}\nModel: {Model}\nEngine: {EngineCC}, {FuelType}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Time now = new Time(15, 36, 48);
+            Console.WriteLine($"The time is: {now}");
+
+            Car car = new Car("Dacia", "Logan", "Petrol", 1.4);
+            Console.WriteLine($"\n{car}");
         }
     }
 }
