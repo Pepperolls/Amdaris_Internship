a3efda9a8455a9bf6809e58022458ad0f6556565
Pepperolls <rares.mladin@gmail.com>
2021-08-02 20:02:52 +0300

    Created the Car class, along with its fields, methods, and a getter/setter for the fuel field.

---
+++ Classes in C# - assignment/ClassesAssignment/ClassesAssignment/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ClassesAssignment
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private string fuelType;
+        private uint engineCc;
+        private uint fuel;
+        private uint mileage;
+        private bool isEngineStarted;
+
+        public Car(String make, String model, String fuelType, uint engineCc)
+        {
+            this.make = make;
+            this.model = model;
+            this.fuelType = fuelType;
+            this.engineCc = engineCc;
+            fuel = 100;
+            mileage = 0;
+            isEngineStarted = false;
+        }
+        public uint Fuel
+        {
+            get
+            {
+                Console.WriteLine("Fuel left:");
+                return fuel;
+            }
+            set
+            {
+                fuel = value;
+            }
+        }
+        public bool HasEnoughFuel()
+        {
+            return fuel > 0;
+        }
+
+        public void StartEngine()
+        {
+            if(HasEnoughFuel())
+            {
+                isEngineStarted = true;
+                fuel--;
+            }
+            else
+            {
+                Console.WriteLine("Please refuel!");
+            }
+        }
+
+        public void StopEngine()
+        {
+            isEngineStarted = false;
+        }
+
+        public void Drive()
+        {
+            if (isEngineStarted)
+            {
+                if (HasEnoughFuel())
+                {
+                    fuel--;
+                    mileage++;
+                }
+                else
+                {
+                    StopEngine();
+                    Console.WriteLine("Please refuel!");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{make} {model}: {mileage} km, range: {fuel}";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car prototype = new Car("Audi", "A6", "Petrol", 3000);
+            Console.WriteLine(prototype);
+
+            Console.WriteLine(prototype.Fuel);
+            prototype.Fuel = 70;
+            Console.WriteLine(prototype.Fuel);
+        }
+    }
+}
