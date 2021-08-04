d7d0a92d9fe05df39a347fc386af2d2ee2ea2adf
Pepperolls <rares.mladin@gmail.com>
2021-08-04 23:09:49 +0300

    Created a custom exception

--- Exception handling & debugging - assignment/Assignment/Exceptions/Program.cs
+++ Exception handling & debugging - assignment/Assignment/Exceptions/Program.cs
@@ -4,7 +4,7 @@ namespace Exceptions
 {
     class RandomExceptions
     {
-        public void WithdrawFunds(int balance, int amount)
+        public void WithdrawFunds(uint balance, uint amount)
         {
             if (amount > balance)
                 throw new Exception("Not enough account balance");
@@ -12,12 +12,41 @@ namespace Exceptions
             Console.WriteLine($"New account balance: {balance}");
         }
     }
+
+    class Person
+    {
+        private uint age;
+
+        public Person(uint age)
+        {
+            this.age = age;
+        }
+        public void HaveADrink()
+        {
+            if (age < 18)
+                throw new PersonIsNotAnAdultException("Person is not an adult!");
+            Console.WriteLine("Enjoy your drink!");
+        }
+    }
+    public class PersonIsNotAnAdultException : Exception
+    {
+        public PersonIsNotAnAdultException() { }
+
+        public PersonIsNotAnAdultException(string message) : base(message) { }
+
+        public PersonIsNotAnAdultException(string message, Exception inner) : base(message, inner) { }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             RandomExceptions a = new RandomExceptions();
-            a.WithdrawFunds(10, 11);
+            a.WithdrawFunds(10, 10); //Modify the 2nd parameter over 10 to see the exception occur.
+
+            Person p = new Person(17);
+            p.HaveADrink();
         }
     }
 }
