5c796cc439bf2b71ab2d70b6b7c0ceaaece29229
Pepperolls <rares.mladin@gmail.com>
2021-08-04 18:41:28 +0300

    Created a method which throws an exception based on its input

---
+++ Exception handling & debugging - assignment/Assignment/Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class RandomExceptions
+    {
+        public void WithdrawFunds(int balance, int amount)
+        {
+            if (amount > balance)
+                throw new Exception("Not enough account balance");
+            balance -= amount;
+            Console.WriteLine($"New account balance: {balance}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomExceptions a = new RandomExceptions();
+            a.WithdrawFunds(10, 11);
+        }
+    }
+}
