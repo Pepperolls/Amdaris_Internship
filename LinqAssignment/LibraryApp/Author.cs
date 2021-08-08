f3d2ff3ca305c0ed08a2ffc95beb0474c3bb20ce
Pepperolls <rares.mladin@gmail.com>
2021-08-08 21:16:47 +0300

    Created the basic classes

---
+++ LinqAssignment/LibraryApp/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Author
+    {
+        public Author(int id, string name, DateTime birthDate, List<string> publishedBooks)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.PublishedBooks = publishedBooks;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public DateTime BirthDate { get; }
+        public List<string> PublishedBooks { get; }
+    }
+}
---
+++ LinqAssignment/LibraryApp/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    class Book
+    {
+        public Book(int id, string title, DateTime publishDate, Author author, List<string> categories)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.PublishDate = publishDate;
+            this.Author = author;
+            this.Categories = categories;
+        }
+
+        public int Id { get; }
+        public string Title { get; }
+        public DateTime PublishDate { get; }
+        public Author Author { get; }
+        public List<string> Categories { get; }
+    }
+}
