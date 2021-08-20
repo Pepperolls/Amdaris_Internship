831964c8f4f94802067a1d318a4b98cd483efc5a
Pepperolls <rares.mladin@gmail.com>
2021-08-20 12:08:11 +0300

    Created, copied and deleted some files

---
+++ Files Assignment/Files Assignment/Files Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileSystemAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo current = new DirectoryInfo(".");
+
+            Console.WriteLine(current.FullName);
+
+
+            string[] animals =
+            {
+                "Dog",
+                "Cat",
+                "Horse",
+                "Fish"
+            };
+
+            string desktopFilePath1 = @"C:\Users\User\Desktop\animals1.txt";
+            string desktopFilePath2 = @"C:\Users\User\Desktop\animals2.txt";
+
+            if (!File.Exists(desktopFilePath1))
+            {
+                File.WriteAllLines(desktopFilePath1, animals);
+                Console.WriteLine("File has been created");
+            }
+            else
+            {
+                File.Copy(desktopFilePath1, desktopFilePath2);
+                File.Delete(desktopFilePath1);
+                Console.WriteLine("File has been copied to another location and deleted");
+            }
+        }
+    }
+}
