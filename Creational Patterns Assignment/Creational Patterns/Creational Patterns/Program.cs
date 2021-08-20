87e1d3710bdd4d0ece91a714beded90c6444b0af
Pepperolls <rares.mladin@gmail.com>
2021-08-20 13:55:58 +0300

    Created a singleton example

---
+++ Creational Patterns Assignment/Creational Patterns/Creational Patterns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Creational_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.Instance;
+            singleton.writeName();
+        }
+
+        public sealed class Singleton
+        {
+            private string Name;
+            private static readonly Singleton instance = new Singleton("Rares");
+
+            static Singleton() { }
+
+            private Singleton(string name) 
+            {
+                Name = name;
+            }
+
+            public static Singleton Instance
+            {
+                get
+                {
+                    return instance;
+                }
+            }
+            public void writeName()
+            {
+                Console.WriteLine(instance.Name);
+            }
+        }
+    }
+}
