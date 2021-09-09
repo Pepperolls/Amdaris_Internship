0b425cd229fda7da35a8972dc50e789099c4d4e6
Pepperolls <rares.mladin@gmail.com>
2021-09-09 19:17:54 +0300

    Finished version

--- Communication Protocols Assignment/index.js
+++ Communication Protocols Assignment/index.js
@@ -1,43 +1,57 @@
 async function getFirstUser() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/users/1'
+    );
 
-  var data = await response.json();
+    var data = await response.json();
 
-  console.log(data);
-  show(data);
-}
+    console.log(data);
 
-getFirstUser();
+    let div = `<div>
+                <p>Id: ${data.id} </p>
+                <p>Name: ${data.name}</p>
+                <p>Username: ${data.username}</p>
+                <p>Email: ${data.email}</p>
+                <p>Street: ${data.address.street}</p>
+                <p>Suite: ${data.address.suite}</p>
+                <p>City: ${data.address.city}</p>
+                <p>Zipcode: ${data.address.zipcode}</p>
+                <p>Phone: ${data.phone}</p>
+                <p>Website: ${data.website}</p>
+                <p>Company Name: ${data.company.name}</p>
+               </div>`;
 
-function show(data) {
-  let tab = `<tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Email</th>
-          <th>Street</th>
-          <th>Suite</th>
-          <th>City</th>
-          <th>Zipcode</th>
-          <th>Phone</th>
-          <th>Website</th>
-          <th>Company Name</th>
-         </tr>`;
-
-  for (let r of data.list) {
-    tab += `<tr> 
-        <td>${r.id} </td>
-        <td>${r.name}</td>
-        <td>${r.username}</td> 
-        <td>${r.email}</td>
-        <td>${r.address.street}</td>    
-        <td>${r.address.suite}</td>    
-        <td>${r.address.city}</td>    
-        <td>${r.address.zipcode}</td>    
-        <td>${r.phone}</td>    
-        <td>${r.website}</td>    
-        <td>${r.company.name}</td>    
-    </tr>`;
+    document.getElementById('firstUser').innerHTML = div;
+  } catch (error) {
+    return new Error(error);
   }
-  document.getElementById('user').innerHTML = tab;
 }
+
+async function getFirstFiveToDos() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+    var data = await response.json();
+
+    console.log(data);
+    let div = `<div></div>`;
+
+    for (var toDo of data) {
+      if (toDo.id > 5) break;
+
+      div += `<p>Id: ${toDo.id}</p>
+      <p>Title: ${toDo.title}</p>`;
+    }
+
+    document.getElementById('firstFiveToDos').innerHTML = div;
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
+document.getElementById('getFirstUser').addEventListener('click', getFirstUser);
+
+document
+  .getElementById('getFirstFiveToDos')
+  .addEventListener('click', getFirstFiveToDos);
